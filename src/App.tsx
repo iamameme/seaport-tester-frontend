1cdf694b49d8408d81f3497017aaca96e06c2b6a
steven <steven@s5stratos.com>
2022-03-09 14:39:38 -0600

    Styles update

--- src/App.tsx
+++ src/App.tsx
@@ -45,88 +45,103 @@ class App extends React.Component {
     };
     const ethAdd = "0xd7e1bc51cd3f30e21b17bab33d77078e3fb7cc26";
     return (
-      <ThemeProvider theme={theme}>
-    <div className="App" style={{ padding: 50, background: "#282c34"}}>
+    <div className="App" style={{ }}>
           <header className="App-header">
-            <h2>Demo NFT Marketplace Aggregator</h2>
-        <div style={{ width: '100%'}}>
-        <Accordion>
-          <AccordionSummary
-            expandIcon={<ExpandMoreIcon />}
-            aria-controls="panel1a-content"
-            id="panel1a-header"
-          >
-            <Typography>Opensea - ETH and Polygon NFTs</Typography>
-          </AccordionSummary>
-          <AccordionDetails>
-            <nft-card contractAddress="0x495f947276749ce646f68ac8c248420045cb7b5e" tokenId="84457751043465328619057449989245392658276532746991168004376457849476957077505"> </nft-card>
+          <div style={{
+            width: '100%', borderBottom: '1px solid #e0e0e3', background: 'white',
+                  flexDirection: 'column',
+    display: 'flex',
+            padding: 15, 
+          }}>
+            <img style={{
+              width: 70,
+              left: 20,
+              position: 'absolute'}} src={require('./images/transparentlogo.png')} />
+            <h2 style={{
+              color: 'purple',
+              fontFamily: 'Space Mono',
+            }}>Demo NFT Marketplace Aggregator</h2>
+          </div>
+          <div style={{ width: '100%' }}>
+            <div style={{ background: '#f4f5f7'}}>
+              <Typography style={{ fontWeight: 500,
+              padding: 10,
+              fontSize: 24,
+                color: 'black'
+              }}>Opensea - ETH and Polygon NFTs</Typography>
+              <Typography style={{ color: 'rgb(123, 128, 154)'}} variant="body2">This shows Opensea NFTs with links to Opensea</Typography>
 
-          </AccordionDetails>
-        </Accordion>
-        <Accordion>
-          <AccordionSummary
-            expandIcon={<ExpandMoreIcon />}
-            aria-controls="panel2a-content"
-            id="panel2a-header"
-          >
-            <Typography>Rarible - ETH, Flow, and Tezos NFTs</Typography>
-          </AccordionSummary>
-          <AccordionDetails>
-            <rarible-card
-              itemId="0xbfb3e424acb4624167581e3a58dbf3ff81bc0eab:17988252055586131602068576116627035873235234406925022496179774906860079939606"
+            <div style={{ display: 'flex', overflowX: 'scroll',  }}>
+                {['84457751043465328619057449989245392658276532746991168004376457849476957077505', '84457751043465328619057449989245392658276532746991168004376457817591119872001', '84457751043465328619057449989245392658276532746991168004376457847277933821953'].map(x => (<nft-card style={{margin: 40}} contractAddress="0x495f947276749ce646f68ac8c248420045cb7b5e" tokenId={x}> </nft-card>))}
+              </div>
+            </div>
+            <div style={{ background: 'white' }}>
+              <Typography style={{
+                fontWeight: 500,
+                padding: 10,
+                fontSize: 24,
+                color: 'black'
+              }}>Rarible - ETH, Flow, and Tezos NFTs</Typography>
+              <Typography style={{ color: 'rgb(123, 128, 154)' }} variant="body2">This shows Rarible NFTs with the ability to buy a Rarible NFT without leaving the site</Typography>
+
+              <div style={{ display: 'flex', overflowX: 'scroll',  }}>
+
+                {['0xbfb3e424acb4624167581e3a58dbf3ff81bc0eab:17988252055586131602068576116627035873235234406925022496179774906860079939606', '0x3ffdf4ca0c52c284f290ccd12a6a1985312e7d0e:17988252055586131602068576116627035873235234406925022496179774906860079939643', '0xbfb3e424acb4624167581e3a58dbf3ff81bc0eab:17988252055586131602068576116627035873235234406925022496179774906860079939634', '0xbfb3e424acb4624167581e3a58dbf3ff81bc0eab:17988252055586131602068576116627035873235234406925022496179774906860079939636'].map(x => (<React.Fragment><rarible-card
+              itemId={x}
               showBuyNow="true"
               env="dev"
-            >
-            </rarible-card>
-          </AccordionDetails>
-        </Accordion>
-        <Accordion>
-          <AccordionSummary
-            expandIcon={<ExpandMoreIcon />}
-            aria-controls="panel3a-content"
-            id="panel3a-header"
-          >
-            <Typography>Known Origin - Ethereum</Typography>
-          </AccordionSummary>
-          <AccordionDetails>
-            <KnownOriginCards ethAdd={ethAdd} />
-          </AccordionDetails>
-              </Accordion>
-              <Accordion>
-                <AccordionSummary
-                  expandIcon={<ExpandMoreIcon />}
-                  aria-controls="panel3a-content"
-                  id="panel3a-header"
-                >
-                  <Typography>Mintable - ETH or Immutable X</Typography>
-                </AccordionSummary>
-                <AccordionDetails>
-                  <MintCards />
-                </AccordionDetails>
-              </Accordion>
-        <Accordion>
-          <AccordionSummary
-            expandIcon={<ExpandMoreIcon />}
-            aria-controls="panel3a-content"
-            id="panel3a-header"
-          >
-            <Typography>Opensea Buy in Site Demo</Typography>
-          </AccordionSummary>
-          <AccordionDetails>
-            <OpenSeaCards />
-          </AccordionDetails>
-        </Accordion>
+              style={{margin: 20}}
+            /></React.Fragment>))}
+                </div>
+            </div>
+            <div style={{ background: '#f4f5f7' }}>
+              <Typography style={{
+                fontWeight: 500,
+                padding: 10,
+                fontSize: 24,
+                color: 'black'
+              }}>Known Origin - Ethereum</Typography>
+              <Typography style={{ color: 'rgb(123, 128, 154)' }} variant="body2">This shows Rarible NFTs with the ability to buy a Rarible NFT without leaving the site</Typography>
+              <Typography style={{ color: 'rgb(123, 128, 154)' }} variant="body2">This shows Known Origin NFTs with links to Known Origin</Typography>
+
+              <div style={{ display: 'flex', overflowX: 'scroll', }}>
+                <KnownOriginCards ethAdd={ethAdd} />
+              </div>
+             </div>
+
+            <div style={{ background: 'white' }}>
+   
+              <Typography style={{
+                fontWeight: 500,
+                padding: 10,
+                fontSize: 24,
+                color: 'black'
+              }}>Mintable - ETH or Immutable X</Typography>
+              <Typography style={{ color: 'rgb(123, 128, 154)' }} variant="body2">This shows Mintable NFTs with links to Mintable</Typography>
+
+              <div style={{ display: 'flex', overflowX: 'scroll', }}>
+                <MintCards />
+              </div>
+            </div>
+            
+            <div style={{ background: '#f4f5f7' }}>
+  
+              <Typography style={{
+                fontWeight: 500,
+                padding: 10,
+                fontSize: 24,
+                color: 'black'
+              }}>Opensea Buy in Site Demo</Typography>
+              <Typography style={{ color: 'rgb(123, 128, 154)' }} variant="body2">These are Opensea NFTs with a way to buy/sell without leaving the site</Typography>
+
+              <div style={{ display: 'flex', overflowX: 'scroll', }}>
+                <OpenSeaCards />
+              </div>
+            </div>
+
         </div>
-        
-       
-        {/*<iframe
-          onLoad={ () => onLoadIframe() }
-          width='100%'
-          height='1000px'
-        frameborder='0' src="https://opensea.io/assets/0x495f947276749ce646f68ac8c248420045cb7b5e/85879054167812751372043786273165907612585945114286779657838364578449698324481?embed=true%22" />*/}
       </header>
         </div>
-        </ThemeProvider>
   );
 }
 }
@@ -161,7 +176,7 @@ class KnownOriginCards extends React.Component<OriginProps, OriginState> {
       query: `{\n  tokens(where: {currentOwner_in: [\"${this.props.ethAdd}\"]}) {\n    id\n    lastSalePriceInEth\n    lastTransferTimestamp\n    metadata {\n      name\n      description\n      image\n    }\n  }\n}\n`
     }).then(resp => {
       this.setState({
-        cards: resp.data.data.tokens.splice(0,2),
+        cards: resp.data.data.tokens.splice(0,4),
       })
     })
   }
@@ -174,15 +189,15 @@ class KnownOriginCards extends React.Component<OriginProps, OriginState> {
       <div className="realcontainer" style={{
         display: 'flex',
         width: '100%',
-        height: 800,
-        overflow: 'hidden'}}>
+        overflowX: 'scroll',
+        overflowY: 'hidden'}}>
         {this.state.cards.map(x => {
           return (
             <div className="container">
         <img className="picture" src={x.metadata.image} />
         <div className="details">
                 <h4>{ x.metadata.name}</h4>
-                <p style={{overflow: 'hidden', height: 200}}>{x.metadata.description}</p>
+                <p style={{overflow: 'hidden', height: 200}}>{x.metadata.description.slice(0,200)}</p>
             <div className="content">
                 <div className="price">
                     <div className="icone__ethereum"></div>
@@ -190,10 +205,6 @@ class KnownOriginCards extends React.Component<OriginProps, OriginState> {
                 </div>
                 
             </div>
-        </div>
-        <div className="creater">
-            <div className="avatar"></div>
-            <p>Creation of <span>Jules Wyvern</span></p>
         </div>
     </div>
    
@@ -208,7 +219,7 @@ type MintProps = {
   //ethAdd: string;
 }
 type MintState = {
-  card?: MintToken;
+  cards?: MintToken[];
 }
 type MintToken = {
   name: string,
@@ -224,41 +235,38 @@ class MintCards extends React.Component<MintProps, MintState> {
   }
 
   componentDidMount() {
-    Axios.get("https://f4r75vsf8k.execute-api.us-east-1.amazonaws.com/prod/tokens/0x892848074ddea461a15f337250da3ce55580ca85:9389?network=1").then(resp => {
-      console.log(resp)
+    const tokens = ['cX-JCCx2jhDTGoS', '0x892848074ddea461a15f337250da3ce55580ca85:9389', 'o2cn_zeQFgd2LMV']
+    Axios.all(tokens.map(x => (Axios.get(`https://f4r75vsf8k.execute-api.us-east-1.amazonaws.com/prod/tokens/${x}?network=1`)))).then(resps => {
       this.setState({
-        card: resp.data
+        cards: [resps[0].data, resps[1].data, resps[2].data],
       })
-    })
+    });
   }
 
   render() {
-    if (!this.state.card ) {
+    if (!this.state.cards) {
       return <div />
     }
     return (
       <div className="realcontainer" style={{
         display: 'flex',
         width: '100%',
-        height: 800,
         overflow: 'hidden'
       }}>
-        <div className="container" >
-          <img className="picture" src={this.state.card.image} />
-          <div className="details">
-            <h4>{this.state.card.name}</h4>
-            <p style={{ overflow: 'hidden', height: 200 }}>{this.state.card.description}</p>
-            <div className="content">
-              <div className="price">
-                <div className="icone__ethereum"></div>
+        {this.state.cards.map(x => (
+          <div className="container" >
+            <img className="picture" src={x.image} />
+            <div className="details">
+              <h4>{x.name}</h4>
+              <p style={{ overflow: 'hidden', height: 200 }}>{x.description}</p>
+              <div className="content">
+                <div className="price">
+                  <div className="icone__ethereum"></div>
+                </div>
               </div>
             </div>
           </div>
-          <div className="creater">
-            <div className="avatar"></div>
-            <p>Creation of <span>Jules Wyvern</span></p>
-          </div>
-        </div>
+        ))}
 
       </div>
     );
@@ -290,7 +298,6 @@ class OpenSeaCards extends React.Component<OpenProps, OpenState> {
   }
 
   componentDidMount() {
-
     Axios.get(`https://api.opensea.io/api/v1/asset/${contractAddress}/${tokenId}`).then(resp => {
       this.setState({
         card: resp.data,
@@ -306,24 +313,19 @@ class OpenSeaCards extends React.Component<OpenProps, OpenState> {
       <div className="realcontainer" style={{
         display: 'flex',
         width: '100%',
-        height: 800,
         overflow: 'hidden'
       }}>
-        <h4>Click on This</h4>
             <div className="container" style={{ cursor: 'pointer'}} onClick={() => this.setState({ open: true})}>
               <img className="picture" src={this.state.card.image_url} />
               <div className="details">
-                <h4>{this.state.card.name}</h4>
+            <h4>{this.state.card.name}</h4>
+            <h4>CLICK ON THIS CARD</h4>
                 <p style={{ overflow: 'hidden', height: 200 }}>{this.state.card.description}</p>
                 <div className="content">
                   <div className="price">
                     <div className="icone__ethereum"></div>
                   </div>
                 </div>
-              </div>
-              <div className="creater">
-                <div className="avatar"></div>
-                <p>Creation of <span>Jules Wyvern</span></p>
               </div>
             </div>
         <Dialog
